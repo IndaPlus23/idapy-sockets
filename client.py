cafa2dc42469c99744da899931b2758be4101b49
Ida <idapy@kth.se>
2023-11-14 16:45:55 +0100

    code from template

---
+++ client.py
@@ -0,0 +1,38 @@
+import threading
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(("127.0.0.1", 55555)) #Connect the client ot the server
+
+nickname = input("Choose a nickname: ")
+def recieve():
+
+    while True:
+
+        try:
+
+            message = client.recv(1024).decode('ascii')
+            if message == "NICK":
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+
+        except:
+
+            print("An error has occurred!")
+            client.close()
+            break
+
+def write():
+
+    while True:
+
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('ascii'))
+
+recieve_thread = threading.Thread(target=recieve)
+recieve_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
+
