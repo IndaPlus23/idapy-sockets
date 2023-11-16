18bf7087a99dc43e6a6ff957ab77cec91327bdd9
Ida <idapy@kth.se>
2023-11-16 13:07:30 +0100

    implement multiple chatrooms

--- server.py
+++ server.py
@@ -1,64 +1,65 @@
 import threading
 import socket
 
-host = "127.0.0.1"  # Localhost
+host = "127.0.0.1"
 port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((host, port))
+server.listen()
 
-server.listen()  # Listen for incoming connections
+clients = []
+nicknames = []
+chatrooms = {'All': [], 'Main': []}
 
-clients = []  # New clients in this list
-nicknames = []  # Nicknames of the clients in this list
-
-
-def broadcast(message):
-    for client in clients:
-        client.send(message)  # Broadcas a message to all the clients in the client list
-
-
-def handle(client):  # Run the function on all client to get the messages
-
-    while True:
-
-        try:  # If you get a message send to all
-
-            message = client.recv(1024)
-            broadcast(message)
-
+def broadcast(message, chatroom):
+    for client in chatroom:
+        try:
+            client.send(message)
         except:
+            pass
 
+def handle(client, room):
+    while True:
+        try:
+            message = client.recv(1024)
+            broadcast(message, chatrooms[room])
+        except:
             index = clients.index(client)
             clients.remove(client)
             client.close()
             nickname = nicknames[index]
-            broadcast(f'{nickname} left the chat!'.encode('ascii'))  # If someone leaves the chat
+            broadcast(f'{nickname} left the chat!'.encode('ascii'), chatrooms[room])
             nicknames.remove(nickname)
             break
 
-
-def recieve():  # Recieve new clients
-
+def receive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
-        client.send('NICK'.encode('ascii'))
+        # Send acknowledgment for NICK
+        client.send('ACK_NICK'.encode('ascii'))
+
         nickname = client.recv(1024).decode('ascii')
         nicknames.append(nickname)
         clients.append(client)
 
+        # Send acknowledgment for ROOM
+        client.send('ACK_ROOM'.encode('ascii'))
+
+        room = client.recv(1024).decode('ascii')
+        if room not in chatrooms:
+            chatrooms[room] = []
+
+        chatrooms[room].append(client)
+        print(f'These are the chatrooms: {chatrooms}!')
+
         print(f'Nickname of new client is {nickname}!')
-        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+        broadcast(f'{nickname} joined the chat!'.encode('ascii'), room)
         client.send('Connected to the server!'.encode('ascii'))
 
-        thread = threading.Thread(target=handle, args=(client,))
+        thread = threading.Thread(target=handle, args=(client, room))
         thread.start()
 
-
-recieve()
-
-
-
+receive()
