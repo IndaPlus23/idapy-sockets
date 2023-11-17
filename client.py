74ddb132470ab85661589caa1048e914fb8149dc
Ida <idapy@kth.se>
2023-11-17 11:17:32 +0100

    create gui

--- client.py
+++ client.py
@@ -1,47 +1,106 @@
-import threading
+import tkinter as tk
+from tkinter import scrolledtext
+from tkinter import messagebox
+from threading import Thread
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(("127.0.0.1", 55555))
 
-nickname = input("Choose a nickname: ")
-room = input("Which chatroom do you want to join? (Default is named 'All') ")
+class ChatClientGUI:
+    def __init__(self, root):
 
-# Receive acknowledgment for NICK
-client.recv(1024)
+        self.root = root
+        self.root.title(str(room)) #Creates a window named after the chat room
 
-# Send nickname
-client.send(nickname.encode('ascii'))
 
-# Receive acknowledgment for ROOM
-client.recv(1024)
+        self.message_entry = tk.Entry(root, width=50) #Create a text box where you can write messages
+        self.message_entry.bind("<Return>", self.send_message) #Sends message if you click enter
 
-# Send room
-client.send(room.encode('ascii'))
+        self.chat_display = scrolledtext.ScrolledText(root, width=60, height=20) #Creates a text window with scrollable text
+        self.chat_display.configure(state='disabled') #Makes sure you cant change anything by typing in the window
 
-def receive():
-    while True:
+        self.disconnect_button = tk.Button(root, text="Disconnect", state='disabled',
+                                           command=self.disconnect_from_server) #Button that disconnects you
+
+        # Send button
+        self.send_button = tk.Button(root, text="Send", state='disabled', command=self.send_message) #Send if you press
+
+
+        # Formatting of the window components
+        self.message_entry.grid(row=2, column=0, padx=10, pady=10)
+        self.chat_display.grid(row=0, column=0, padx=10, pady=10)
+        self.disconnect_button.grid(row=1, column=0, padx=10, pady=5)
+        self.send_button.grid(row=2, column=1, padx=10, pady=10)
+
+        # Initialize all the variables
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.nickname = nickname
+        self.room = room
+
+        self.client_socket.connect(("127.0.0.1", 55555)) #Connect to the correct server
+
+        self.client_socket.recv(1024)
+        self.client_socket.send(self.nickname.encode('ascii'))
+        self.client_socket.recv(1024)
+        self.client_socket.send(self.room.encode('ascii'))
+
+        self.disconnect_button['state'] = 'normal'
+        self.send_button['state'] = 'normal'
+
+        self.receive_thread = Thread(target=self.receive)
+        self.receive_thread.start()
+
+    def disconnect_from_server(self):
         try:
-            message = client.recv(1024).decode('ascii')
-            print(message)
+            self.client_socket.close()
+
+            #Disable all buttons
+            self.disconnect_button['state'] = 'disabled'
+            self.send_button['state'] = 'disabled'
+
+
+        except Exception as e:
+            messagebox.showerror("Error", f"Error during disconnection: {str(e)}")
+
+    def send_message(self, event=None):
+        try:
+            message = f'{self.nickname}: {self.message_entry.get()}' #Message format
+
+            self.client_socket.send(message.encode('ascii'))
+            self.message_entry.delete(0, 'end')
+
+        except Exception as e:
+            messagebox.showerror("Error", f"Error sending message: {str(e)}")
+
+    def receive(self):
+        try:
+            while True:
+                message = self.client_socket.recv(1024).decode('ascii')
+                self.display_message(message)
         except:
-            print("An error has occurred!")
-            client.close()
-            break
+            messagebox.showinfo("Disconnected", "You have been disconnected from the server.")
+            self.disconnect_from_server()
 
-def write():
-    while True:
-        writing = input('')
-        if writing.lower() == 'exit':
-            leave()
-        message = '{}: {}'.format(nickname, writing)
-        client.send(message.encode('ascii'))
+    def display_message(self, message):
+        self.chat_display.configure(state='normal')
+        self.chat_display.insert('end', message + '\n')
+        self.chat_display.yview('end')
+        self.chat_display.configure(state='disabled')
 
-def leave():
-    client.close()
+    def leave(self):
+        self.client_socket.close()
+        self.root.destroy()
 
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
 
-write_thread = threading.Thread(target=write)
-write_thread.start()
+if __name__ == "__main__":
+
+    root = tk.Tk()
+
+    #LEt use choose nickname and room
+    nickname = input("Choose a nickname: ")
+    room = input("Which chatroom do you want to join? ")
+
+    gui = ChatClientGUI(root)
+    gui.nickname = nickname
+    gui.room = room
+
+    root.mainloop()
